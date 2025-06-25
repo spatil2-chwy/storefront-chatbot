2bf6d3681620f094bbe1d61dca3bcdc6461b78c0
jepshtein <jepshtein@chewy.com>
2025-06-25 09:45:08 -0400

    Updated user model

--- server/src/models/user.py
+++ server/src/models/user.py
@@ -1,10 +1,15 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel
+from datetime import datetime
+from typing import List, Optional
+from product import Product 
+from pet import PetProfile
 
 class User(BaseModel):
-    id: int
-    email: EmailStr
-    name: str
-
-class UserCreate(BaseModel):
-    email: EmailStr
-    name: str
+    customer_id: str
+    operating_revenue: float
+    first_placed_order: datetime
+    zip_code: str
+    city: str
+    state: str
+    orders: List[Product] = []
+    pets: List[PetProfile] = []
