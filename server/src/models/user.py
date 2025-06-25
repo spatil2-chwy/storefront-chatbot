538c3fc382f28fb8a13ef1312acc36ad2dc845b3
Sakshee Patil <spatil2@chewy.com>
2025-06-25 09:59:49 -0400

    fixed user model

--- server/src/models/user.py
+++ server/src/models/user.py
@@ -1,15 +1,10 @@
-from pydantic import BaseModel
-from datetime import datetime
-from typing import List, Optional
-from product import Product 
-from pet import PetProfile
+from pydantic import BaseModel, EmailStr
 
 class User(BaseModel):
-    customer_id: str
-    operating_revenue: float
-    first_placed_order: datetime
-    zip_code: str
-    city: str
-    state: str
-    orders: List[Product] = []
-    pets: List[PetProfile] = []
+    id: int
+    email: EmailStr
+    name: str
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    name: str
