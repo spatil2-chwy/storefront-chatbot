1dbf2ba35297b84e10ec70ab7207e28c64d0e678
Sakshee Patil <spatil2@chewy.com>
2025-07-02 13:09:41 -0400

    updated chromadb builder for category tags

--- scripts/productdbbuilder.py
+++ scripts/productdbbuilder.py
@@ -175,13 +175,13 @@ for row in product_rows:
 
     # Add category tags
     if row["CATEGORY_LEVEL1"]:
-        for tag in map(str.strip, row["CATEGORY_LEVEL1"].split(',')):
-            if tag:
-                metadata[f"categorytag1:{tag}"] = True
+        tag = row["CATEGORY_LEVEL1"].strip()
+        if tag:
+            metadata[f"categorytag1:{tag}"] = True
     if row["CATEGORY_LEVEL2"]:
-        for tag in map(str.strip, row["CATEGORY_LEVEL2"].split(',')):
-            if tag:
-                metadata[f"categorytag2:{tag}"] = True
+        tag = row["CATEGORY_LEVEL2"].strip()
+        if tag:
+            metadata[f"categorytag2:{tag}"] = True
 
     documents.append(clean_name)
     metadatas.append(metadata)
