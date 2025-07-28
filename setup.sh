a43386a0801fb3a7ca298b25207e917ed5d49cf6
Ryan Gilbert <rgilbert1@chewy.com>
2025-07-28 15:53:27 -0400

    setup commands file

---
+++ setup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Navigate to project root
+cd storefront-chatbot
+
+# Create and activate virtual environment
+python3 -m venv venv
+source venv/bin/activate
+
+# Install Python dependencies
+pip install -r server/requirements.txt
+
+# git checkout dev [REMOVE!!!]
+
+# Unzip data files
+unzip data/backend/articles/all_wp_posts.json.zip -d data/backend/articles/ 
+unzip data/backend/products/item_df.csv.zip -d data/backend/products
+unzip data/backend/reviews/all_chewy_products_with_qanda.csv.zip -d data/backend/reviews
+unzip data/backend/reviews/results.jsonl.zip -d data/backend/reviews
+
+# Load data
+cd server
+python src/load_data.py
+
+# Run custom scripts
+cd ../scripts
+python run_all_data_loading.py
+python generate_landing_products.py
+
+# Build frontend
+cd ../client
+npm install
+npm run build
